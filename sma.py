d8af823af21a30a3beaf34ab68d41694ae3e02c6
Herlanzin <91843322+Herlanzin@users.noreply.github.com>
2021-10-03 16:09:30 -0300

    Add files via upload

---
+++ sma.py
@@ -0,0 +1,15 @@
+from time import sleep
+import os
+for b in range(0, 100):
+	print (f'994{b}5{b}9{b}' )
+	sleep(0.1)
+	os.system('clear')
+	print (f'CONSULTADO ESSE NUNBER')
+	print (f' Inválido')
+os.system ('clear')
+sleep (1)
+print ('nunber valido')
+print ('aqui o nunber ')
+input ('DIGITE ENTER : ')
+print ('whatsapp go sms ')
+print ('sms ')
